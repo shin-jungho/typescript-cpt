a43fcae49b1bca07b78b6c70dab60013cd2c936d
jungho shin <wjdghtls11@gmail.com>
2022-01-21 17:39:30 +0900

    Fix types to interface Prac

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,11 @@
-let todoItems: { id: number; title: string; done: boolean }[];
-
+let todoItems: Todo[];
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
 // api
-function fetchTodoItems(): {id: number; title: string; done: boolean}[] {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -16,7 +20,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: {id: number, title: string, done: boolean}): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -24,7 +28,7 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: {id: number, title: string, done: boolean}): void {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -59,6 +63,6 @@ function log(): void {
   console.log(todoItems);
 }
 
-todoItems:  = fetchTodoItems();
+todoItems = fetchTodoItems();
 addTwoTodoItems();
 log();

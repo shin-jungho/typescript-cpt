47a58407b9f3e2cc19b4159c1ecc12d1689e29dd
jungho shin <wjdghtls11@gmail.com>
2022-01-21 16:27:23 +0900

    typescript function Prac

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -7,3 +7,20 @@ let num: number = 10;
 let arr: Array<number> = [1, 2 ,3];
 let heroes: Array<string> =  ['Capt', 'Thor', 'Hulk'];
 let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//   name: 'capt',
+//   age: 100
+// };
+let person: { name: string, age: number } ={
+  name: 'thor',
+  age: 1000
+}
+
+// TS 진위갑
+let show: boolean = true;
